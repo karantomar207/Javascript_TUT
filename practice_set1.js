9741114e0fb7085a5a05802d60775ac7c6790545
Karan Singh Tomar <61387213+karantomar207@users.noreply.github.com>
2022-11-11 20:13:07 +0530

    Initial Commit
    Some Basic JS Question for clearing concept.

---
+++ practice_set1.js
@@ -0,0 +1,35 @@
+// Q-1 Add a number to a String
+
+const a="Karan";
+console.log(a+""+1);
+
+
+const b=1;
+console.log(a+b);
+
+
+
+// Q-2 Datatype of variable
+
+console.log(typeof(a));
+
+
+
+// Q-3  hold a number later-Not possible
+
+let x={
+    name:"Karan",
+    age:"21",
+    number:"9321930201"
+}
+
+console.log(x);
+
+
+// Q-5 -Add new object to the variable
+x['friend']="Dipansh";
+x['name']="Shiva";
+console.log(x);
+console.log(x['name'])              // or console.log(x[.name]) 
+  
+ 
