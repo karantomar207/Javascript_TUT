8fcb8a1ac70b095b908a65302198c0aa9a72c718
Karan Singh Tomar <61387213+karantomar207@users.noreply.github.com>
2022-11-12 19:26:40 +0530

    First Commit

---
+++ practiceset2.js
@@ -0,0 +1,44 @@
+// Q-1  Use L-operator to  check number lies between range or not
+
+// const a=10;
+
+// if(a>=10 && a<=20){
+//     console.log("Yes")
+// }else{
+//     console.log("No")
+// }
+
+
+// let a=10;
+
+// switch(a){
+//     case 10:
+//         console.log("Your age is 10");
+//         break
+//     case 11:
+//         console.log("your age is 11")
+//         break
+
+//     default :
+//         console.log("Your age is different")
+// }
+
+
+
+// Q-3 Number is divsible by 2 & 3
+
+// const a=12;
+
+// if(a%2==0 && a%3==0){
+//     console.log("Yes")
+// }else{
+//     console.log("No")
+// }
+
+
+// Q-4  Print two statement on consdition using ternary opertor
+
+// const age=20;
+
+// let a=age>18?"You can drive":"You cannot drive"
+// console.log(a)
---
+++ practiceset3.js
@@ -0,0 +1,22 @@
+// Q-1 Print ther marks of a student in an object using for loop
+
+// let a ={
+//     harry:98,
+//     rohan:90,
+//     akash:70
+// }
+
+// for(let i=0;i<Object.keys(a).length;i++){
+//     console.log("The marks of "+Object.keys(a)[i]+" are "+a[Object.keys(a)[i]]);
+// }
+
+
+// Q-3 Mean of Five Numbers
+
+let a=[3,4,5,6,7,8]
+let sum=0;
+
+for(let i=0;i<a.length;i++){
+    sum+=a[i];
+}
+console.log(sum/5);
